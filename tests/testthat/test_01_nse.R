38cba9e4f387741a36d3055cb0c70a45fcbfc7b2
Desi Quintans <me@desiquintans.com>
2019-02-25 17:51:11 +1100

    Add unit tests for special characters in package names
    Bare package names can accept A-Z a-z 0-9 _._~:/. Other characters will require the argument to be passed in a string. Also ensure that nse_dots() can pass strings without changing them in prep for support for some of install_github()'s advanced features.

--- tests/testthat/test_01_nse.R
+++ tests/testthat/test_01_nse.R
@@ -1,28 +1,56 @@
-context("Non-standard evaluation (HEY: Tests should be run from a new R session.)")
+context("Non-standard evaluation (HEY: Tests should be run in a new R session.)")
 
 library(librarian)
 
-test_that("nse_dots() can accept arbitrary number of args", {
+test_that("nse_dots() can accept an arbitrary number of args", {
+    skip_on_cran()
     expect_equal(length(nse_dots()), 0)
     expect_equal(length(nse_dots(dplyr)), 1)
     expect_equal(length(nse_dots(DesiQuintans/desiderata)), 1)
     expect_equal(length(nse_dots(dplyr, DesiQuintans/desiderata)), 2)
+    expect_equal(length(nse_dots(dplyr, DesiQuintans/desiderata, phyloseq)), 3)
 })
 
-test_that("nse_dots() returns a character vector", {
+test_that("nse_dots() should return a character vector", {
+    skip_on_cran()
     expect_type(nse_dots(),                               "character")
     expect_type(nse_dots(dplyr),                          "character")
     expect_type(nse_dots(DesiQuintans/desiderata),        "character")
     expect_type(nse_dots(dplyr, DesiQuintans/desiderata), "character")
 })
 
+test_that("nse_dots() should allow letters, numbers, - . _ ~ : and /", {
+    skip_on_cran()
+    # https://stackoverflow.com/a/7109208/5578429
+    # But not all of the characters are accepted inside R symbols.
+    test <- "A1B2C3D4E5F6G7H8I9J0K-L.M_N~O:P/QRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    
+    expect_identical(nse_dots(A1B2C3D4E5F6G7H8I9J0K-L.M_N~O:P/QRSTUVWXYZabcdefghijklmnopqrstuvwxyz, keep_user = TRUE), 
+                     c("A1B2C3D4E5F6G7H8I9J0K-L.M_N~O:P/QRSTUVWXYZabcdefghijklmnopqrstuvwxyz"))
+})
+
+test_that("A character vector passed to nse_dots() should not be changed even if it has special characters", {
+    skip_on_cran()
+    # https://stackoverflow.com/a/7109208/5578429
+    expect_identical(nse_dots("A1B2C3D4E5F6G7H8I9J0K-L.M_N~O:P/Q?R#S[T]U@V!W$X$Y&Z'a(b)c*d+e,f;g=hijklmnopqrstuvwxyz", keep_user = TRUE),
+                     "A1B2C3D4E5F6G7H8I9J0K-L.M_N~O:P/Q?R#S[T]U@V!W$X$Y&Z'a(b)c*d+e,f;g=hijklmnopqrstuvwxyz")
+})
+
+test_that("A user can pass a mixture of bare names and strings to nse_dots()", {
+    skip_on_cran()
+    expect_identical(nse_dots(barename, bare-name, bare.name, "stringname", "string-name", "string.name"),
+                     c("barename", "bare-name", "bare.name", "stringname", "string-name", "string.name"))
+})
+
 test_that("nse_dots() removes duplicated names", {
+    skip_on_cran()
     expect_equal(length(nse_dots(dplyr, purrr, dplyr)), 2)
     expect_equal(length(nse_dots(DesiQuintans/desiderata, DesiQuintans/desiderata)), 1)
     expect_equal(length(nse_dots(dplyr, dplyr, DesiQuintans/desiderata, DesiQuintans/desiderata)), 2)
 })
 
-test_that("nse_dots() removes username correctly", {
+test_that("nse_dots() removes (or keeps) the username correctly", {
+    skip_on_cran()
     expect_equal(length(nse_dots(dplyr, keep_user = TRUE)), 1)
     expect_equal(length(nse_dots(dplyr, keep_user = FALSE)), 1)
     expect_equal(length(nse_dots(DesiQuintans/desiderata, keep_user = TRUE)), 1)
